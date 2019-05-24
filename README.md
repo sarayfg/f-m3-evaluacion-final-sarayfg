23c9e89b9f3aa279b157b9c891858d0d9b45194c
sarayfg <sarayfernandezgarcia@gmail.com>
2019-05-24 15:22:33 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Ejercicio evaluación modulo 3 - Uso de React
+
+Saray Fernández García
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+  hola mundo
+    </div>
+  );
+}
+
+export default App;
