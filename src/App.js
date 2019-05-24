aea3f83499e8cd05f10152269dce2189f551406b
sarayfg <sarayfernandezgarcia@gmail.com>
2019-05-24 15:52:38 +0200

    get info from API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.scss';
+import fetchHarryPotter from './services/ReasonsService';
 
-function App() {
-  return (
-    <div className="App">
-  hola mundo
-    </div>
-  );
+class App extends React.Component {
+constructor(props) {
+  super(props)
+  this.state = {
+    harryData: [],
+  }
+
+}
+
+componentDidMount(){
+  this.getData();
+}
+
+getData() {
+  fetchHarryPotter()
+    .then(data => console.log(data));
+}
+
+  render () {
+    return (
+      <div className="App">
+        hola mundo
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/services/ReasonsService.js
@@ -0,0 +1,5 @@
+const HarryPotterUrl = 'http://hp-api.herokuapp.com/api/characters';
+
+const fetchHarryPotter = () => fetch(HarryPotterUrl).then(response => response.json());
+
+export default fetchHarryPotter;
