9c22ac1cea2f582ce18f1f77f1b6667e035fec1d
sarayfg <sarayfernandezgarcia@gmail.com>
2019-05-25 21:39:12 +0200

    change img depending of character house

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -2,16 +2,29 @@ import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import gryffindor from '../../images/gryffindor.jpg';
+import Hufflepuff from '../../images/Hufflepuff.jpeg';
+import Ravenclaw from '../../images/Ravenclaw.jpeg';
+import Slytherin from '../../images/Slytherin.jpeg';
 
 class CharacterCard extends React.Component {
+
+    chooseImg(item){
+        if(item.house=== 'Gryffindor'){
+            return gryffindor
+        } else if(item.house ===  "Slytherin"){
+            return Slytherin
+        } else if(item.house === "Hufflepuff") {
+            return Hufflepuff
+        } else {return Ravenclaw}
+    }
     render(){
         const {item}= this.props;
         return (
             <Fragment>
             <h2>{item.name}</h2>
             <img src={item.image}></img>
-            <p>{item.house}</p>
-            <img src={gryffindor}></img>
+            <img src={this.chooseImg(item)}></img>
+           
             <Link to={`/characterdetails/${item.id}`}>See more information</Link>
             </Fragment>
         )
